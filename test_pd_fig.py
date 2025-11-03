81b9555ec8bf77bd816ff09f2f7a34f9be667eb3
TrR1s <anatolyturin0505@gmail.com>
2025-11-03 20:17:05 +0300

    clusters

--- test_pd_fig.py
+++ test_pd_fig.py
@@ -7,8 +7,8 @@ from subjects.player import PlrPool,Player
 from subjects.corr_coef import CorrCoeff
 import matplotlib.pyplot as plt
 
-start_date = datetime.strptime("01-7-2022", "%d-%m-%Y")
-end_date = datetime.strptime("01-8-2022", "%d-%m-%Y")
+start_date = datetime.strptime("01-1-2022", "%d-%m-%Y")
+end_date = datetime.strptime("01-1-2025", "%d-%m-%Y")
 plr_pool = PlrPool()
 corr_coef = CorrCoeff()
 pd_fig, plr_pool = do_pd_figures(start_date,end_date,plr_pool,corr_coef)
@@ -20,5 +20,5 @@ plr_pool_json_string = plr_pool.model_dump_json(indent=2)
 plr_pool_frm_1 = PlrPool.model_validate_json(plr_pool_json_string) 
 print(plr_pool_json_string)
 
-# with open("plr_pool.json", 'w') as f:
-#     json.dump(plr_pool_json_string, f, indent=4)
+with open("plr_pool.json", 'w') as f:
+    json.dump(plr_pool_json_string, f, indent=4)
